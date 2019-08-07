483e0e8d9644f1e8c4b80d9ac65b6cabaa99b6ad
Sharath Parsam <sharathparsam@gmail.com>
2019-08-07 18:05:06 +1000

    Add cors support to decode token

--- src/main/java/com/sharath/parsam/controllers/JwtController.java
+++ src/main/java/com/sharath/parsam/controllers/JwtController.java
@@ -13,10 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value = "Exposed JWT Operations")
@@ -29,6 +26,7 @@ public class JwtController {
 
     private Logger log = LoggerFactory.getLogger(JwtController.class);
 
+    @CrossOrigin(maxAge = 3600)
     @RequestMapping(value = DECODE_TOKEN_PATH, method = RequestMethod.POST)
     @ApiOperation(value = "Decode the JWT and display in plain text format")
     @ApiResponses(value = {
